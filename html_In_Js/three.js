b2d5dd749d0a7e8b40c81bfc005ec26649d75ff7
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2022-01-14 01:34:12 +0900

    +

--- html_In_Js/three.js
+++ html_In_Js/three.js
@@ -17,13 +17,12 @@ h1.addEventListener("click", handleTitleClick);
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "active";
   /*
   if (h1.className.contains(clickedclass)) {
     h1.classList.remove(clickedClass);
   } else {
     h1.classList.add(clickedClass);
-  }*/
+  } ===  */
   h1.classList.toggle("active");
 }
 
