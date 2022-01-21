96a67fba96dc5c1cabb12d42f9db9dc367772b13
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2022-01-22 03:12:36 +0900

    logout complet

--- makeApp/js/greetings.js
+++ makeApp/js/greetings.js
@@ -12,6 +12,7 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
+
   /*
   console.log(username);
   console.log(event);
@@ -26,6 +27,7 @@ function onLoginSubmit(event) {
   */
 }
 function paintGreetings(username) {
+  logout.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello ${username}`; //== "Hello " + username; 절대 따옴표 아님``
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -33,6 +35,19 @@ function paintGreetings(username) {
 loginForm.addEventListener("submit", onLoginSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+const logout = document.querySelector("#logout-form");
+
+function onLogoutSubmit() {
+  event.preventDefault();
+  greeting.innerText = ``;
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.removeItem(USERNAME_KEY, username);
+  logout.classList.add(HIDDEN_CLASSNAME);
+}
+
+logout.addEventListener("click", onLogoutSubmit);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
@@ -48,12 +63,3 @@ function handleLinkClick(event) {
 }
 link.addEventListener("click", handleLinkClick);
 */
-
-const logout = document.querySelector(".logout");
-function onLogoutSubmit() {
-  greeting.innerText = ``;
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  localStorage.removeItem(HIDDEN_CLASSNAME);
-}
-
-logout.addEventListener("click", onLogoutSubmit);
